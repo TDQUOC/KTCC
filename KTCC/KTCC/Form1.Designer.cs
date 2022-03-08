fe7e18a719c670c021d12e9ab57d7dd150d820f4
TDQUOC <tdq17072000@gmail.com>
2022-03-08 10:00:09 +0700

    edit label

--- KTCC/KTCC/Form1.Designer.cs
+++ KTCC/KTCC/Form1.Designer.cs
@@ -38,11 +38,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(98, 70);
+            this.label1.Location = new System.Drawing.Point(69, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.Size = new System.Drawing.Size(86, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
+            this.label1.Text = "Tên đăng nhập";
             // 
             // txtUserName
             // 
@@ -66,7 +66,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Password";
+            this.label2.Text = "Mật khẩu";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // btnlogin
